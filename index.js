c667dbd7c16678b04726e8375889e11be1eb5a0a
unknown <seedownthesun@gmail.com>
2020-06-19 09:25:43 -0400

    circle generation complete. beginning early game mechanics

--- index.js
+++ index.js
@@ -1,56 +1,24 @@
+
+//define svg for use
 const svg = document.querySelector('svg')
-const circle = document.querySelector('circle')
 
-
-// //going into dom to get radius value
-let radius = parseFloat(circle.getAttribute('r'));
-
-// initialize turn attr
-let turn;
-
-//////////////////////////////////////////////
-///  change radius and then deletes child  ///
-//////////////////////////////////////////////
-
-const bigC = setInterval(() => {
-    switch(radius) {
-        default:
-        //if turn has occored, circle will shrink
-            if(turn) circle.setAttribute('r', radius--);
-            //else radius grows
-            else circle.setAttribute('r', radius++);
-            break;
-        
-    //if circle is 100, shrink radius
-        case 100:
-            turn=true //
-            circle.setAttribute('r', radius--) //drop radius below 100 to break out of this case
-            break;
-        
-    //once radius hits zero, remove circle from svg 
-        case 0:
-            //remove child circle from svg
-            svg.removeChild(circle);
-            clearInterval(bigC);
-            break;
-        }
-//create a speed interval? random number between 5 - 15
-    }, 10);
-
-//adds new circle after 1 second
-setTimeout(() => {
-    const circle2 = document.createElementNS("http://www.w3.org/2000/svg", "circle");
-    circle2.setAttribute("cx", "20");
-    circle2.setAttribute("cy", "20");
-    circle2.setAttribute("r", "15");
-    svg.appendChild(circle2);
-}, 1000);
-
+//////////////////
+///  function  ///
+//////////////////
 
 //function to get random number from 1 to max number, second parameter sets minimum
-function randomNumber(max, min=1) {  
+function randomNumber(max, min=1) {
     return Math.floor(Math.random() * (max - min) + min); 
-}  
+}
+
+//x
+//min this.absoluteMaxRadius
+//max svg.getAttribute("width") - this.absoluteMaxRadius
+
+//y
+//min this.absoluteMaxRadius
+//max svg.getAttribute("height") - this.absoluteMaxRadius
+
 
 ///////////////////////////////
 ///  create a circle class  ///
@@ -59,35 +27,111 @@ function randomNumber(max, min=1) {
 class Circle{
     //may not need constructor if there is nothing passed
     constructor(){
-    this.radius = randomNumber(300, 50)
+    
+    //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+    //  DEFINE RADIUS OF GENERATED CIRCLES   //
+    //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+    this.aboluteMaxRadius = 120
+    this.aboluteMinRadius = 70
+    this.maxRadius = randomNumber(this.aboluteMaxRadius, this.aboluteMinRadius)
+    console.log(parseInt(svg.getAttribute("width")) - this.aboluteMaxRadius)
+    console.log(this.aboluteMaxRadius)
+    
+    const { aboluteMaxRadius } = this;
+
     //random location in current browser
-    this.cx = randomNumber(window.innerWidth)
-    this.cy = randomNumber(window.innerHeight)
-    //still need to set color and speed?
+    this.cx = randomNumber((parseInt(svg.getAttribute("width")) - aboluteMaxRadius), aboluteMaxRadius)
+    this.cy = randomNumber((parseInt(svg.getAttribute("height")) - aboluteMaxRadius), aboluteMaxRadius)
+
+    // this.cx = randomNumber((1000 - 120), 120)
+    // this.cy = randomNumber((500 - 120), 120)
+
+    //assign the element a random id
+    this.id = randomNumber(9).toString()+randomNumber(9).toString()+randomNumber(9).toString()
+
+    //create default element circle 
+    this.newCircle = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+
+    //initialize for later use
+    this.targetCircle;
+    this.reachedApex;
+
+    //////////////////////////////////////
+    //still need to set color and speed
+    //////////////////////////////////////
 
     }
     //method to drop circle onto svg
     addCircleToSVG = () => {
-        const newCircle = document.createElementNS("http://www.w3.org/2000/svg", "circle");
-        newCircle.setAttribute("cx", this.cx);
-        newCircle.setAttribute("cy", this.cy);
-        newCircle.setAttribute("r", this.radius);
+        const { cx, cy, id, newCircle } = this;
+        newCircle.setAttribute("cx", cx);
+        newCircle.setAttribute("cy", cy);
+        newCircle.setAttribute("id", id);
+        //defualt is always 1
+        newCircle.setAttribute("r", "1");  
+        
+        newCircle.setAttribute("fill", "red");
+        
         svg.appendChild(newCircle);
     }
-}
+    growShrinkRemove = () => {
+
+        //run method to add circle into SVG
+        this.addCircleToSVG()
+
+        //target newly added circle
+        this.targetCircle = document.getElementById(this.id)
+    
+        const intervalFunc = setInterval(() => {
+            
+            //deconstruct variables we will need
+            const { currentRadius, maxRadius, reachedApex, targetCircle } = this;
+
+            switch(currentRadius) {
+                default:
+                //if radius has reached highest point/apex, circle will shrink
+                    if(reachedApex) targetCircle.setAttribute('r', currentRadius-1);
+                    //else radius grows
+                    else targetCircle.setAttribute('r', currentRadius+1);
+                    break;
+                
+            //if circle is 100, shrink radius
+                case (maxRadius):
+                    //redefining and not using it as a variable so I do need to use this
+                    this.reachedApex=true
+
+                    targetCircle.setAttribute('r', currentRadius-1) //drop radius below 100 to break out of this case
+                    break;
+                
+            //once radius hits zero, remove circle from svg 
+                case 0:
+                    //remove child circle from svg
+                    svg.removeChild(targetCircle);
+                    //break out of interval
+                    clearInterval(intervalFunc);
+                    break;
+                }
+            //~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+            //  DEFINE SPEED OF CIRCLE   //
+            //~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+            }, randomNumber(30, 20));
+        };
+        get currentRadius() {
+            return parseInt(this.targetCircle.getAttribute("r"));
+        };
+};
 
 //create circle class
 //create method to append to svg
 //remove once the cycle is complete
 
-const c1 = new Circle()
+const firstCircle = new Circle();
+firstCircle.growShrinkRemove();
 
-
-/* 
-1. get coordinate cx cy of spawn point
-2. have circle expand and then contract based on max point
- --- color should be randomized
-*/
+const circleFactory = setInterval(() => {
+    const endlessCircle = new Circle();
+    endlessCircle.growShrinkRemove();
+}, 4000)
 
 
 //// CREATE ARRAYS, probably don't need
