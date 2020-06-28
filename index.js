0415f2dc88574e49720b60e5000281642db1ac96
unknown <seedownthesun@gmail.com>
2020-06-28 16:17:48 -0400

    finished levelManager class and added first sound

--- index.js
+++ index.js
@@ -1,44 +1,6 @@
 //define svg for use
 const svg = document.querySelector('svg')
 const score = document.querySelector('#score')
-const currentLevel = document.querySelector('#level').innerHTML
-const background = document.querySelector('rect')
-
-// background.setAttribute("fill", "yellow");
-
-//array of random colors
-const colors = ['rgba(142, 191, 63, .5)', 'rgba(191, 63, 65, .5)', 'rgba(63, 84, 191, .5)'];
-
-//level variables that will set the parameters of each level
-//  ex.   levels[currentLevel-1].nextLevel
-
-const levels = [
-    // level 1
-    {
-        nextLevel: 1500,
-        levelColors: ['rgba(142, 191, 63, .5)', 'rgba(191, 63, 65, .5)', 'rgba(63, 84, 191, .5)'],
-        levelBackgroundColor: "rgba(255,0,0,0.1)",
-        minGrowthSpeed: 50,
-        maxGrowthSpeed: 40,
-        minSpawnSpeed: 10,
-        maxSpawnSpeed: 30
-    },
-    // level 2
-    {
-        nextLevel: 3000,
-        levelColors: ['rgba(142, 191, 63, .8)', 'rgba(191, 63, 65, .8)', 'rgba(63, 84, 191, .8)'],
-        levelBackgroundColor: "pink" ,
-        minGrowthSpeed: 50,
-        maxGrowthSpeed: 40,
-        minSpawnSpeed: 10,
-        maxSpawnSpeed: 30
-    },
-    {
-        nextLevel: 5000,
-        levelColors: ['rgba(142, 191, 63, .6)', 'rgba(191, 63, 65, .6)', 'rgba(63, 84, 191, .6)'],
-        levelBackgroundColor: "green" 
-    }
-]
 
 ///  FUNCTIONS  ///
 
@@ -47,35 +9,116 @@ function randomNumber(max, min=1) {
     return Math.floor(Math.random() * (max - min) + min); 
 }
 
-// NEXT LEVEL LOGIC //
-function triggerNextLevel() {
-    if(score.innerHTML >= 1500) {
-        level.innerHTML = 2;
-    } 
-    if(level.innerHTML === '2') {
-        document.body.style.backgroundColor = 'red';
+/////////////////////////////
+///  LEVEL MANAGER CLASS  ///
+/////////////////////////////
+
+class LevelManager{
+    constructor(){
+        this.targetLevel = document.querySelector('#level')
+        this.targetBackground = document.querySelector('rect')
+
+        this.progression = [
+            //level 1 goal
+            200, 
+            //level 2 goal
+            500, 
+            //level 3 goal
+            600
+        ]
     }
-}
+    updateProgress = () => {
+        //if the player's score is greater than or equal to the current progression
+        if(score.innerHTML >= this.progression[this.currentLevel - 1]) {
+            //level goes up by 1
+            this.currentLevel = this.currentLevel + 1;
+        };
+    };
+    //change background color
+    changeBackground = color => {
+        this.targetBackground.setAttribute('fill', color)
+    };
+    get currentLevel() {
+        return parseInt(this.targetLevel.innerHTML);
+    }
+    set currentLevel(value) {
+        this.targetLevel.innerHTML = value;
+    }
+};
 
 ///  CIRCLE CLASS  ///
 
 class Circle{
-    constructor(){ //may not need constructor if there is nothing passed
+    constructor(currentLevel){ //may not need constructor if there is nothing passed
+
+    this.levelIndex = currentLevel - 1
+
+    //array defining the min and max Growth speeds for the circle elements.
+    //the smaller the number, the faster the circle balloons
+    this.sizeRange = [
+        //level 1
+        {
+            maxRadius: 120, 
+            minRadius: 70
+        }, 
+        //level 2
+        {
+            maxRadius: 100, 
+            minRadius: 60
+        },
+        //level 3
+        {
+            maxRadius: 90, 
+            minRadius: 40
+        }
+    ]
+
+    //smaller number is faster speed!
+    this.spawnSpeed = [
+        //level 1
+        {
+            highest: 50, 
+            lowest: 70
+        },
+        //level 2
+        {
+            highest: 45, 
+            lowest: 60
+        },
+        //level 3
+        {
+            highest: 40, 
+            lowest: 45
+        },
+    ]
+
+    //color of circles changes based on level
+    this.colors = [
+        //level 1
+        ['rgba(142, 191, 63, .5)', 'rgba(191, 63, 65, .5)', 'rgba(63, 84, 191, .5)'],
+        //level 2
+        ['red', 'green', 'yellow'],
+        //level 3
+        ['rgba(142, 191, 63, .7)', 'rgba(191, 63, 65, .7)', 'rgba(63, 84, 191, .7)']
+    ]
+
+    const { sizeRange, levelIndex, spawnSpeed, colors } = this;
 
     // constant variables //
-    this.aboluteMaxRadius = 120
-    this.aboluteMinRadius = 70
+    this.aboluteMaxRadius = sizeRange[levelIndex].maxRadius
+    this.aboluteMinRadius = sizeRange[levelIndex].minRadius
     this.newCircle = document.createElementNS("http://www.w3.org/2000/svg", "circle"); //create default element circle
-    
+
+    const { aboluteMaxRadius, aboluteMinRadius } = this;
+
     //randomized variables //
-    this.maxRadius = randomNumber(this.aboluteMaxRadius, this.aboluteMinRadius)
-    this.randomColor = colors[randomNumber(3,0)];
+    this.maxRadius = randomNumber(aboluteMaxRadius, aboluteMinRadius)
+    this.randomColor = colors[levelIndex][randomNumber(3,0)];
     this.id = randomNumber(9).toString()+randomNumber(9).toString()+randomNumber(9).toString() //assign the element a random id
     
     //change speed 
-    this.speed = randomNumber(30, 10)
+    this.speed = randomNumber(spawnSpeed[levelIndex].highest, spawnSpeed[levelIndex].lowest);
 
-    const { aboluteMaxRadius } = this; //deconstruct for convenience
     this.cx = randomNumber((parseInt(svg.getAttribute("width")) - aboluteMaxRadius), aboluteMaxRadius) //random location svg. configured to not touch edge
     this.cy = randomNumber((parseInt(svg.getAttribute("height")) - aboluteMaxRadius), aboluteMaxRadius)
 
@@ -85,7 +128,7 @@ class Circle{
     }
     //method to drop circle onto svg
     addCircleToSVG = () => {
-        const { cx, cy, id, newCircle, randomColor } = this;
+        const { cx, cy, id, newCircle, randomColor, clickCircle } = this;
         newCircle.setAttribute("cx", cx);
         newCircle.setAttribute("cy", cy);
         newCircle.setAttribute("id", id);
@@ -93,13 +136,9 @@ class Circle{
         newCircle.setAttribute("fill", randomColor);
         
         svg.appendChild(newCircle); //add circle to svg
-        
-        ////these didn't work for some reason
-        //svg.insertBefore(newCircle, svg.firstChild);
-        //svg.insertAdjacentElement('afterbegin', newCircle);
 
-        this.targetCircle = document.getElementById(this.id) //target newly added circle
-        this.targetCircle.addEventListener('click', this.clickCircle);
+        this.targetCircle = document.getElementById(id) //target newly added circle
+        this.targetCircle.addEventListener('click', clickCircle); //apply event listener
     }
     growShrinkRemove = () => {
         this.addCircleToSVG() //run method to add circle into SVG
@@ -107,12 +146,7 @@ class Circle{
         const intervalFunc = setInterval(() => {
             const { currentRadius, maxRadius, reachedApex, targetCircle } = this; //deconstruct variables we will need
 
-// if currentRadius is less than or equal to zero then removechild
-// if reachedApex = true then currentRadius -1
-// else currentRadius + 1
-
-// LAST CHECK if current radius is greater than or equal to maxradius then reachedApex is true
-
+//reconfigure to if else statement
             switch(currentRadius) {
                 default:
                     if(reachedApex) targetCircle.setAttribute('r', currentRadius-1); //if radius reaches highest point, circle shrinks
@@ -126,8 +160,8 @@ class Circle{
                     try {
                         svg.removeChild(targetCircle); //remove child circle from svg
                         clearInterval(intervalFunc); //break out of interval
-                        score.innerHTML = score.innerHTML - 50;
-                        if (score.innerHTML <= 0) score.innerHTML = 0;
+                        score.innerHTML = score.innerHTML - 50; //lose 50 points if you don't catch the circle
+                        if (score.innerHTML <= 0) score.innerHTML = 0; //score can not dip below 0
                     } catch (error) {
                         "error: already deleted"
                     }
@@ -139,6 +173,16 @@ class Circle{
             }, this.speed);
         };
         clickCircle = () => {
+            
+            var sound = document.querySelector('#sound')
+            var soundFlag = true;
+            if (soundFlag) {
+                sound.pause();
+                sound.currentTime = 0;
+                sound.play();
+                soundFlag = false;
+            }
+            
             score.innerHTML = parseInt(score.innerHTML) + (this.aboluteMaxRadius - this.currentRadius);
             const { intervalFunc, targetCircle } = this;
             clearInterval(intervalFunc);
@@ -146,26 +190,26 @@ class Circle{
         }
         get currentRadius() {
             return parseInt(this.targetCircle.getAttribute("r"));
-
         }
-
 };
 
-// get currentLevel() {
-//     return parseInt(document.querySelector('#level'));
-// }
+///////////////////////////////
+///  initiate levelManager  ///
+///////////////////////////////
+
+const levelManager = new LevelManager()
 
 //////////////////////////
 ///  initiate circles  ///
 //////////////////////////
 
 //create one initially otherwise there is 4 second delay
-const firstCircle = new Circle();
+const firstCircle = new Circle(levelManager.currentLevel);
 firstCircle.growShrinkRemove();
 
 //create new circle every 2 seconds
 const circleFactory = setInterval(() => {
-    const endlessCircle = new Circle();
+    const endlessCircle = new Circle(levelManager.currentLevel);
     endlessCircle.growShrinkRemove();
-    triggerNextLevel();
+    levelManager.updateProgress();
 }, 2000)
